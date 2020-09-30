7f5c85e78d7a7a09ec4e2d38fec501d373bbcccf
maximLomako <maxim.lomako@icloud.com>
2020-09-30 17:25:29 +0300

    init: init Accordion project

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is APP</h1>
+    </div>
+  );
+}
+
+function Rating() {
+  return (
+    <div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+    </div>
+  )
+}
+
+function Accordion() {
+  return (
+    <div>
+      <h3>Меню</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+
+
+
+export default App;
