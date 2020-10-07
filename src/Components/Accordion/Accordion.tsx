faba9058273a46aa629e0f5a59ac3dd4fb0baaf1
maximLomako <maxim.lomako@icloud.com>
2020-10-07 13:45:57 +0300

    ref: refactor accordion and rating components

--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -7,23 +7,12 @@ type AccordionPropsType = {
 
 
 function Accordion(props: AccordionPropsType) {
-
-  if (!props.collapsed) {
-    return (
-      <div>
-        <AccordionTitle title={props.titleValue}/>
-        <AccordionBody/>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <AccordionTitle title={props.titleValue}/>
-      </div>
-    )
-  }
-
-
+  return (
+    <div>
+      <AccordionTitle title={props.titleValue}/>
+      {!props.collapsed && <AccordionBody/>}
+    </div>
+  )
 }
 
 type AccordionTitlePropsType = {
